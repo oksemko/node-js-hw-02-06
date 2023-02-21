e56cc0c3fbc49f08bef04d88f056be45300ad37c
Oksana Semko <osemko@gmail.com>
2023-02-21 12:44:38 +0200

    Added finall changes

---
+++ helpers/isValidParams.js
@@ -0,0 +1,16 @@
+const { isValidObjectId } = require("mongoose");
+const createError = require("./createError");
+
+const isValidParams = (req, res, next) => {
+  const { contactId: id } = req.params;
+  const result = isValidObjectId({ id });
+
+  if (!result) {
+    const error = createError(400, "Invalid id");
+
+    return next(error);
+  }
+  next();
+};
+
+module.exports = isValidParams;
