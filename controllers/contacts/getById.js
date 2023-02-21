46dd294b2e70fd4eded33120a903e651dc96e5a6
Oksana Semko <osemko@gmail.com>
2023-02-21 12:40:43 +0200

    Added finall changes

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,12 +1,12 @@
 const { Contact } = require("../../models/contacts");
-const CreateError = require("http-errors");
+const { createError } = require("../../helpers");
 
 const getById = async (req, res) => {
   const { contactId: id } = req.params;
   const result = await Contact.findById({ id });
 
   if (!result) {
-    throw new CreateError(404, "Not found");
+    throw new createError(404);
   }
 
   res.json({
--- controllers/contacts/removeById.js
+++ controllers/contacts/removeById.js
@@ -1,12 +1,12 @@
 const { Contact } = require("../../models/contacts");
-const CreateError = require("http-errors");
+const { createError } = require("../../helpers");
 
 const removeById = async (req, res) => {
   const { contactId: id } = req.params;
   const result = await Contact.findByIdAndRemove({ id });
 
   if (!result) {
-    throw new CreateError(404, "Not found");
+    throw new createError(404);
   }
 
   res.json({
