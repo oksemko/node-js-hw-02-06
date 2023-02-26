dd1016ca052738e2e0be4f66ec70a0bc47704f5c
Oksana Semko <osemko@gmail.com>
2023-02-27 00:36:14 +0200

    Done

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const app = require("./app");
 const mongoose = require("mongoose");
+const app = require("./app");
+
+// In this stage we have warning about Mongoose: the `strictQuery` option
+// will be switched back to `false` by default in Mongoose 7 (now we use dependency "mongoose": "^6.9.1").
+// So we should use `mongoose.set('strictQuery', false);`---> if we want to prepare
+// for this change. Or use `mongoose.set('strictQuery', true); ---> to suppress this warning.
+mongoose.set("strictQuery", true);
 
 const { DB_HOST, PORT = 3000 } = process.env;
+console.log(DB_HOST);
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
-    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
   })
   .catch((error) => {
     console.log(error.message);
