2c0af49adfd677797a44d52565899d1c1d6ae6ce
Oksana Semko <osemko@gmail.com>
2023-03-09 10:48:10 +0200

    Transfered authenticate to middlewares

--- helpers/index.js
+++ helpers/index.js
@@ -2,12 +2,11 @@ const createError = require("./createError");
 const controllerWrapper = require("./controllerWrapper");
 const validation = require("./validation");
 const isValidParams = require("./isValidParams");
-const authenticate = require("./authenticate");
+
 
 module.exports = {
   createError,
   controllerWrapper,
   validation,
   isValidParams,
-  authenticate,
 };
