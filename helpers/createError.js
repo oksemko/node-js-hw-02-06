d5c2804e9bbfed58219561f8185eb5d83e56ec01
Oksana Semko <osemko@gmail.com>
2023-03-10 00:48:58 +0200

    Added info

--- helpers/createError.js
+++ helpers/createError.js
@@ -13,3 +13,29 @@ const createError = (status, message = messages[status]) => {
 };
 
 module.exports = createError;
+
+// 1. Інформаційні 100 - 199
+// 100: Continue
+
+// 2. Успішні 200 - 299
+// 200: OK
+// 201: Created
+// 202: Accepted
+// 204: No Content
+
+// 3. Перенаправлення 300 - 399
+// 301: Moved Permanently
+// 307: Temporary Redirect
+
+// 4. Клієнтські помилки 400 - 499
+// 400: Bad Request
+// 401: Unauthorized
+// 403: Forbidden
+// 404: Not Found
+
+// 5. Серверні помилки 500 - 599
+// 500: Internal Server Error
+// 501: Not Implemented
+// 502: Bad Gateway
+// 503: Service Unavailable
+// 504: Gateway Timeout
