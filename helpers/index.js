b2524d40833bd77e5275d27963a87c02bc52df1a
Oksana Semko <osemko@gmail.com>
2023-03-13 02:31:50 +0200

    Added sendMail

--- helpers/index.js
+++ helpers/index.js
@@ -2,11 +2,12 @@ const createError = require("./createError");
 const controllerWrapper = require("./controllerWrapper");
 const validation = require("./validation");
 const isValidParams = require("./isValidParams");
-
+const sendMail = require("./sendMail");
 
 module.exports = {
   createError,
   controllerWrapper,
   validation,
   isValidParams,
+  sendMail,
 };
